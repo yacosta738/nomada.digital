d34621790773512bfa1a8bb8634989ee30b3fb85
Yuniel Acosta Pérez <yuniel.acosta@cav.desoft.cu>
2021-01-11 14:48:04 -0500

    first commit portfolio

---
+++ create-git-hook.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+baseDir=`pwd`
+filename="$baseDir/.git/hooks/pre-commit"
+rm -rf $filename
+printf "\033[0;32mCreate git hooks for pre-commit changes an create docs folder for updates to GitHub...\033[0m\n"
+printf "$filename"
+{
+        echo "#!/bin/bash"
+        echo "ROOT_REPO=$baseDir"
+        echo 'hugo -s $ROOT_REPO'
+        echo 'git add $ROOT_REPO'
+} >> $filename
+
+chmod +x $filename
