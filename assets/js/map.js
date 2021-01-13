4c4184922518fcf32a512680c6e301e2177c537b
Yuniel Acosta Pérez <yuniel.acosta@cav.desoft.cu>
2021-01-12 22:43:45 -0500

    Big changes in all website

---
+++ assets/js/map.js
@@ -0,0 +1,47 @@
+    /* ==============================================
+    MAP -->
+    =============================================== */
+
+    const locations = [['<div class="infobox"><h3 class="title"><a href="#">OUR USA OFFICE</a></h3><span>NEW YORK CITY 2045 / 65</span><span>+90 555 666 77 88</span></div>',
+        52.370216,
+        4.895168,
+        2]];
+    const map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 14,
+            scrollwheel: false,
+            navigationControl: true,
+            mapTypeControl: false,
+            scaleControl: false,
+            draggable: true,
+            styles: [{
+                "featureType": "administrative",
+                "elementType": "labels.text.fill",
+                "stylers": [{
+                    "featureType": "poi.business",
+                    "elementType": "geometry.fill",
+                    "stylers": [{"visibility": "on"}]
+                }]
+            }
+            ],
+            center: new google.maps.LatLng(52.370216, 4.895168),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+    );
+    const infowindow = new google.maps.InfoWindow();
+    let marker,
+        i;
+    for (i=0;
+    i < locations.length;
+    i++) {
+        marker=new google.maps.Marker( {
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]), map: map, icon: '/images/loaders/loader-realestate.gif'
+        }
+        );
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        }
+        )(marker, i));
+    }
